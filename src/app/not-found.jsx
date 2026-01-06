d3ba072ec75eaf1d2bbf4077f22ed1d2abf79537
Kunj Savaliya <kunj.bvminfotech@gmail.com>
2026-01-06 16:58:37 +0530

    Fix responsive 404 page image and button

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -11,22 +11,22 @@ function Page404() {
   }, []);
     return (
         <>
-           <div className="relative w-full h-[50dvh] md:h-[90dvh] xl:h-[100dvh]  overflow-hidden bg-[#110f27]" >
+           <div className="relative w-full h-[55dvh] sm:h-[70dvh] md:h-[90dvh] xl:h-[100dvh]  overflow-hidden bg-[#110f27]" >
       
       {/* Full Image */}
       <img
         src="/HomePages/2x.png"
         alt="404 Not Found"
-        className="w-full h-full object-fill md:object-none xl:object-fill"
+        className="w-full h-full object-contain md:object-none xl:object-fill"
       />
 
       {/* Button Overlay */}
-      <div className="absolute inset-0 flex items-end justify-center pb-14 sm:pb-20 md:pb-28 ">
+      <div className="absolute inset-0 flex items-end justify-center  pb-14 ">
         <Link href="/">
            <Button
                         text="Go to Home page"
                         iconColor="#6C63FF"
-                        className="w-full  md:text-xl font-semibold py-3 text-white  rounded-md hover:bg-[#5961F9]/10 transition duration-300 mt-2 sm:mt-5   bg-transparent border border-[#5961F9] bg-[#110F27]"
+                        className="w-full  font-semibold py-3 text-white  rounded-md hover:bg-[#5961F9]/10 transition duration-300   bg-transparent border border-[#5961F9] bg-[#110F27]"
                       >
         
                       </Button>
