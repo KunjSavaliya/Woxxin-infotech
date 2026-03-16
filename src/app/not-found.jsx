69d142bbe41a6e54f9057ee75505f372455f45ba
KunjSavaliya <kunj.bvminfotech@gmail.com>
2026-03-16 09:27:14 +0530

    Initial commit

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,35 +1,35 @@
 "use client";
 import React, { useEffect } from "react";
-
 import Button from "@/app/Reusable/Button";
-
 import Link from "next/link";
+
 function Page404() {
   useEffect(() => {
     document.title = "Not Found - Woxxin Solution";
   }, []);
-  return (
-    <>
-      <div className="relative w-full h-[55dvh] sm:h-[70dvh] md:h-[90dvh] xl:h-[100dvh]  overflow-hidden bg-[#110f27]">
-        {/* Full Image */}
-        <img
-          src="/HomePages/2x.png"
-          alt="404 Not Found"
-          className="w-full h-full object-contain md:object-none xl:object-fill"
-        />
 
-        {/* Button Overlay */}
-        <div className="absolute inset-0 flex items-end justify-center  pb-14 ">
-          <Link href="/">
-            <Button
-              text="Go to Home page"
-              iconColor="#6C63FF"
-              className="w-full  font-semibold py-3 text-white  rounded-md hover:bg-[#5961F9]/10 transition duration-300   bg-transparent border border-[#5961F9] bg-[#110F27]"
-            ></Button>
-          </Link>
-        </div>
-      </div>
-    </>
+  return (
+    <div className="relative w-full h-screen overflow-hidden bg-[#110f27] flex items-center justify-center">
+
+      {/* Image */}
+      <img
+        src="/HomePages/2x.png"
+        alt="404 Not Found"
+        className="absolute inset-0 w-full h-full object-fill"
+      />
+
+      {/* Button */}
+      <div className="absolute bottom-8 w-full flex justify-center ">
+        <Link href="/" className=" flex justify-center" >
+          <Button
+            text="Go to Home page"
+            iconColor="#6C63FF"
+            className="w-auto flex items-center justify-center gap-3 text-white font-semibold py-3 rounded-xl border border-[#5961F9] hover:bg-[#5961F9] transition bg-[#110F27]"
+          />
+        </Link>
+      </div >
+
+    </div >
   );
 }
 
